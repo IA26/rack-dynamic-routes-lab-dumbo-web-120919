4f5aa1f0cb4adc1e6edb1ab30dd7005a9ab4fedb
Isaac Avilez <isaac1180@aol.com>
2020-01-28 18:49:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,11 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/"
-      resp.write "You requested the songs"
+    if req.path.match(/items/)
+        resp.write "You requested the songs"
     else
-      resp.write "Path not found"
-      resp.status 404
+        resp.write "Path not found"
+        resp.status 404
     end 
     resp.finish 
   end 
