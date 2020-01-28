6a95631713a8d19ca81d5d8b811da4a5544f0d0d
Isaac Avilez <isaac1180@aol.com>
2020-01-28 18:51:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,23 @@
 class Application
 
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-        resp.write "You requested the songs"
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
     else
-        resp.write "Path not found"
-        resp.status 404
-    end 
-    resp.finish 
-  end 
+      resp.status=404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+
 end
