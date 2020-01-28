c769874641061786a28fd67bb0a6064be57fe25c
Isaac Avilez <isaac1180@aol.com>
2020-01-28 18:52:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.find{|i| i.name == item_name}
+      if @@items.find{|item| i.name == item_name}
         resp.write item.price
       else 
         resp.status = 400
